47d4367e1c8780f3f08073a2e9a82e176d3437ec
office oveis <oveis@gmail.com>
2022-11-21 13:01:03 +0330

    version1

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 class HelloWorld{
         public static void main(String args[]){
-                        System.out.println("hello world");
+                        System.out.println("hello world version1");
         }
 }
 
